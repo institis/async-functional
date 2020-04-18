66f4b085a1c2c034d75a510230caf9e4406ac7cd
Sajanikar (EXT), Yogesh <yogesh.sajanikar.ext@siemens.com>
2020-04-18 09:23:55 +0530

    Fix property based tests
    - Fixed property based testing for of/collect

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,9 @@
 const winston = require('winston');
 const logging = require('./src/logging/index.ts');
 
-const console = new winston.transports.Console();
 const logFiles = new winston.transports.File({
   level: 'debug',
   filename: 'test.log',
 });
-console.log('Adding logger');
-logging.logger.add(console).add(logFiles);
+
+logging.logger.add(logFiles);
